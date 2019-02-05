c9058f57ca1351a5f920adb736228f07e83ebd70
Aluno <Aluno@SN-279251>
2019-02-05 19:50:30 -0200

    criado a interface grafica criado a classe de calculo do imc tratamento dos textos das labels

---
+++ src/calculoimc/Imc.java
@@ -0,0 +1,15 @@
+
+package calculoimc;
+
+
+public class Imc {
+    public double calcular(double peso, double altura){
+        return peso / (altura*altura);   
+    }
+    
+     
+        
+       
+    
+    
+}
